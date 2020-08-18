e07f18c57f4ca5baf28aa72ff280135fc83a8aab
wwology <wwologic@gmail.com>
2020-08-18 22:14:52 +0700

    ListView.builder

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -10,11 +10,40 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  void _openEndDrawer() {
+    _scaffoldKey.currentState.openEndDrawer();
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenHeight = MediaQuery.of(context).size.height;
     var screenWidth = MediaQuery.of(context).size.width;
+
+    List categories = [
+      {
+        'imagePath': 'assets/Icons/CheeseCake.png',
+        'space': screenHeight / 10,
+      },
+      {
+        'imagePath': 'assets/Icons/Jars.png',
+        'space': screenHeight / 10,
+      },
+      {
+        'imagePath': 'assets/Icons/MilkBrownies.png',
+        'space': screenHeight / 10,
+      },
+      {
+        'imagePath': 'assets/Icons/Bombolini.png',
+        'space': screenHeight / 10,
+      },
+    ];
+
+    List isSelected = [true, false, false, false];
+
     return Scaffold(
+      key: _scaffoldKey,
       endDrawer: Drawer(),
       body: Row(
         children: [
@@ -26,59 +55,44 @@ class _HomeScreenState extends State<HomeScreen> {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Padding(
-                  padding: const EdgeInsets.only(top: 80),
-                  child: InkWell(
+                  padding: EdgeInsets.only(top: screenHeight / 20),
+                  child: Material(
+                    color: kMainThemeColour,
                     child: IconButton(
+                      splashColor: Colors.transparent,
+                      splashRadius: 30,
                       icon: Image.asset('assets/Icons/MenuButton.png'),
                       onPressed: () {},
                     ),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30),
-                  child: IconButton(
-                    icon: Icon(Icons.search),
-                    iconSize: 50,
-                    onPressed: () {},
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(top: 100),
-                  // To make the text Vertical, Rotated Box Quarter Turns 3
-                  child: RotatedBox(
-                    quarterTurns: 3,
-                    child: DefaultTabController(
-                      length: 4,
-                      child: Row(
-                        children: [
-                          CategoryItem(
-                            imagePath: "assets/Icons/Bombolini.png",
-                            isActive: false,
-                            press: () {},
-                            space: 80,
-                          ),
-                          CategoryItem(
-                            imagePath: "assets/Icons/MilkBrownies.png",
-                            isActive: false,
-                            press: () {},
-                            space: 80,
-                          ),
-                          CategoryItem(
-                            imagePath: "assets/Icons/Jars.png",
-                            isActive: false,
-                            press: () {},
-                            space: 80,
-                          ),
-                          CategoryItem(
-                            imagePath: "assets/Icons/CheeseCake.png",
-                            isActive: true,
-                            press: () {},
-                            space: 0,
-                          ),
-                        ],
+                Align(
+                  child: Padding(
+                    padding: EdgeInsets.only(top: screenHeight / 30),
+                    child: Material(
+                      color: kMainThemeColour,
+                      child: IconButton(
+                        icon: Icon(Icons.search),
+                        splashRadius: 30,
+                        splashColor: Colors.transparent,
+                        iconSize: 40,
+                        onPressed: () {},
                       ),
                     ),
                   ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 10),
+                  child: ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: categories.length,
+                      itemBuilder: (context, index) {
+                        return CategoryItem(
+                          imagePath: categories[index]['imagePath'],
+                          space: categories[index]['space'],
+                        );
+                      }),
                 )
               ],
             ),
@@ -87,11 +101,16 @@ class _HomeScreenState extends State<HomeScreen> {
             children: [
               Align(
                 alignment: Alignment.centerRight,
-                child: Container(
-                    height: 100,
-                    width: 400,
-                    alignment: Alignment.centerRight,
-                    child: Image.asset('assets/JoireLogo.png')),
+                child: GestureDetector(
+                  onTap: () {
+                    _openEndDrawer();
+                  },
+                  child: Container(
+                      height: screenHeight / 6,
+                      width: screenWidth - 60,
+                      alignment: Alignment.centerRight,
+                      child: Image.asset('assets/JoireLogo.png')),
+                ),
               )
             ],
           )
@@ -101,34 +120,53 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class CategoryItem extends StatelessWidget {
+class CategoryItem extends StatefulWidget {
   final String imagePath;
-  final bool isActive;
-  final Function press;
   final double space;
-  const CategoryItem(
-      {Key key, this.imagePath, this.isActive, this.press, this.space})
-      : super(key: key);
+  CategoryItem({Key key, this.imagePath, this.space}) : super(key: key);
+
+  @override
+  _CategoryItemState createState() => _CategoryItemState();
+}
+
+class _CategoryItemState extends State<CategoryItem> {
+  bool isActive;
+  @override
+  void initState() {
+    isActive = false;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: press,
+      onTap: () {
+        isActive
+            ? setState(() {
+                isActive = false;
+              })
+            : setState(() {
+                isActive = true;
+              });
+      },
       child: Padding(
-        padding: EdgeInsets.only(right: space),
+        padding: EdgeInsets.only(top: widget.space),
         child: Column(
           children: [
-            RotatedBox(
-              quarterTurns: 1,
-              child: isActive
-                  ? Container(
-                      height: 50, width: 50, child: Image.asset(imagePath))
-                  : Container(
-                      height: 35,
-                      width: 35,
-                      child: Image.asset(imagePath),
+            isActive
+                ? Container(
+                    height: 80, width: 80, child: Image.asset(widget.imagePath))
+                : Container(
+                    height: 50,
+                    width: 50,
+                    child: ColorFiltered(
+                      colorFilter: ColorFilter.mode(
+                        Colors.grey,
+                        BlendMode.modulate,
+                      ),
+                      child: Image.asset(widget.imagePath),
                     ),
-            ),
+                  ),
           ],
         ),
       ),
